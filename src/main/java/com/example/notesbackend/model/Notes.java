e6d8c497b1bbec74e5bc84b8b1a75fa2e6f53a92
samir <s-malik@live.co.uk>
2023-06-03 15:01:52 +0100

    Updated application properities and Notes with annotation

--- src/main/java/com/example/notesbackend/model/Notes.java
+++ src/main/java/com/example/notesbackend/model/Notes.java
@@ -1,10 +1,8 @@
 package com.example.notesbackend.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+@Entity
 public class Notes {
 
     @Id
